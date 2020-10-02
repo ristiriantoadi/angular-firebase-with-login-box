57cb4d14ecd5f045aa6116d4964bd6c3d2f46078
Ristirianto Adi <ristiriantoadi@gmail.com>
2020-10-03 07:54:31 +0800

    login component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import { AngularFireDatabase,AngularFireList } from '@angular/fire/database';
+import { MatDialog,MatDialogRef } from '@angular/material/dialog';
+import { LoginComponentComponent } from './login-component/login-component.component';
+import { RegisterComponentComponent } from './register-component/register-component.component';
+// import { LoginComponentComponent } from './login-component/login-component.component';
+// import { RegisterComponentComponent } from './register-component/register-component.component';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +13,59 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'firebase-angular';
+
+  updateItemKey
+  updateItemValue
+  itemsRef: AngularFireList<any>;
+  items = [];
+  constructor(db:AngularFireDatabase,public dialog:MatDialog){
+    this.itemsRef=db.list("/items");
+    this.itemsRef.snapshotChanges(['child_added','child_changed','child_moved','child_removed'])
+      .subscribe(actions=>{
+        this.items=[]
+        actions.forEach(action=>{
+          this.items.push({'key':action.payload.key,'value':action.payload.val().value})
+        })
+      })
+  }
+
+  addItem(itemValue){
+    // console.log(item) 
+    this.itemsRef.push({"value":itemValue})   
+  }
+
+  updateInput(item){
+    this.updateItemKey=item.key
+    this.updateItemValue=item.value
+  }
+
+  updateItem(){
+    console.log(this.updateItemKey)
+    this.itemsRef.update(this.updateItemKey,{"value":this.updateItemValue});
+  }
+
+  deleteItem(){
+    this.itemsRef.remove(this.updateItemKey)
+  }
+
+  openLogin(){
+    // open(LoginComponentComponent)
+    const dialogRef = this.dialog.open(LoginComponentComponent, {
+      // width: '400px',
+      // height:'500px'
+      // data: {name: this.name, animal: this.animal}
+    });
+  }
+  openRegister(){
+    // open(RegisterComponentComponent)
+    const dialogRef = this.dialog.open(RegisterComponentComponent, {
+      // width: '250px',
+      // height:'500px'
+      // data: {name: this.name, animal: this.animal}
+    });
+  }
+
+  open(component:Component){
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,45 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+import { environment } from '../environments/environment';
+import {FormsModule} from "@angular/forms";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSliderModule} from '@angular/material/slider';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon'; 
+import {MatButtonModule} from '@angular/material/button'; 
+import {MatDialogModule, MatDialog} from '@angular/material/dialog';
+import { LoginComponentComponent } from './login-component/login-component.component';
+import { RegisterComponentComponent } from './register-component/register-component.component'; 
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponentComponent,
+    RegisterComponentComponent
   ],
+  entryComponents:[LoginComponentComponent,RegisterComponentComponent],
   imports: [
+    MatIconModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatDialogModule,
+    MatButtonModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireDatabaseModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    FormsModule,
+    MatSliderModule,
+    MatToolbarModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
